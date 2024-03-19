d5c4289d53e5bd5702134182ce57567066f35f4e
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2024-03-19 11:58:41 +0530

    done

--- lib/features/splash/view/splash_screen.dart
+++ lib/features/splash/view/splash_screen.dart
@@ -13,24 +13,33 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        extendBody: true,
-        extendBodyBehindAppBar: true,
-        body: Container(
-            width: ScreenUtil().screenWidth,
-            height: ScreenUtil().screenHeight,
-            color: const Color(0xFF090350),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  height: 5.h,
-                ),
-                SizedBox(
-                    height: 221.h,
-                    width: 360.w,
-                    child: Image.asset(AssetsPathes.appLogoImage))
-              ],
-            )));
+    return SafeArea(
+      child: Scaffold(
+          extendBody: true,
+          extendBodyBehindAppBar: true,
+          body: Container(
+              width: ScreenUtil().screenWidth,
+              height: ScreenUtil().screenHeight,
+              color: const Color(0xFF090350),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Welcome to SAT Mobility Solutions",
+                    style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5.h,
+                  ),
+                  SizedBox(
+                      height: 221.h,
+                      width: 360.w,
+                      child: Image.asset(AssetsPathes.appLogoImage))
+                ],
+              ))),
+    );
   }
 }
